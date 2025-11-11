29e9512a547abcce58887c6520cfa2f14ecabcff
minikin <warbles.lieu_04@icloud.com>
2025-11-11 11:26:10 +0800

    chore: fix bugs

--- src/presence.rs
+++ src/presence.rs
@@ -4,11 +4,9 @@
 /// - Some:   field present with value     {"field": value}
 ///
 /// Cardinality for bool: 2 (base) + 1 (optional) + 1 (nullable) = 4 states
-use std::{fmt, iter::FusedIterator, slice::SliceIndex};
+use std::{fmt, iter::FusedIterator};
 
-#[derive(Copy, Debug, Hash)]
-#[derive_const(Eq)]
-#[allow(clippy::derived_hash_with_manual_eq)]
+#[derive(Clone, Copy, Debug, Hash, PartialEq, Eq)]
 pub enum Presence<T> {
     /// Field/key is absent from the structure
     Absent,
@@ -56,8 +54,9 @@ impl<T> Presence<T> {
     #[inline]
     pub const fn as_ref(&self) -> Presence<&T> {
         match *self {
-            Presence::Some(ref val) => Presence(val),
-            _ => None,
+            Presence::Some(ref val) => Presence::Some(val),
+            Presence::Null => Presence::Null,
+            Presence::Absent => Presence::Absent,
         }
     }
 
@@ -67,8 +66,9 @@ impl<T> Presence<T> {
     #[inline]
     pub const fn as_mut(&mut self) -> Presence<&mut T> {
         match *self {
-            Presence::Some(ref mut val) => Presence(val),
-            _ => None,
+            Presence::Some(ref mut val) => Presence::Some(val),
+            Presence::Null => Presence::Null,
+            Presence::Absent => Presence::Absent,
         }
     }
 
@@ -76,7 +76,7 @@ impl<T> Presence<T> {
     /// # Examples
     ///
     #[inline]
-    pub const fn to_nested_option(self) -> Option<Option<T>> {
+    pub fn to_nested_option(self) -> Option<Option<T>> {
         match self {
             Presence::Absent => None,
             Presence::Null => Some(None),
@@ -87,6 +87,56 @@ impl<T> Presence<T> {
     /////////////////////////////////////////////////////////////////////////
     // Getting to contained values
     /////////////////////////////////////////////////////////////////////////
+
+    /// Takes the value out of the Presence, leaving Absent in its place.
+    /// # Examples
+    ///
+    #[inline]
+    pub const fn take(&mut self) -> Presence<T> {
+        let mut slot = Presence::Absent;
+        std::mem::swap(self, &mut slot);
+        slot
+    }
+
+    /// Returns the number of elements in the Presence (0, 1, or possibly 0 for Null/Absent).
+    /// This is primarily for iterator support.
+    /// # Examples
+    ///
+    #[inline]
+    pub const fn len(&self) -> usize {
+        match self {
+            Presence::Some(_) => 1,
+            Presence::Null | Presence::Absent => 0,
+        }
+    }
+
+    /////////////////////////////////////////////////////////////////////////
+    // Iterator constructors
+    /////////////////////////////////////////////////////////////////////////
+
+    /// Returns an iterator over the possibly contained value.
+    /// # Examples
+    ///
+    #[inline]
+    pub const fn iter(&self) -> Iter<'_, T> {
+        Iter {
+            inner: Item {
+                presence: self.as_ref(),
+            },
+        }
+    }
+
+    /// Returns a mutable iterator over the possibly contained value.
+    /// # Examples
+    ///
+    #[inline]
+    pub fn iter_mut(&mut self) -> IterMut<'_, T> {
+        IterMut {
+            inner: Item {
+                presence: self.as_mut(),
+            },
+        }
+    }
 
     /////////////////////////////////////////////////////////////////////////
     // Transforming contained values
@@ -105,27 +155,25 @@ impl<T: fmt::Display> fmt::Display for Presence<T> {
 }
 
 // Default implementation
-impl<T> const Default for Presence<T> {
+impl<T> Default for Presence<T> {
     /// Returns the default Presence value, which is Absent
     /// # Examples
     ///
     fn default() -> Presence<T> {
-        Absent
+        Presence::Absent
     }
 }
 
 // Iterator implementation
 impl<T> IntoIterator for Presence<T> {
     type Item = T;
-    type IntoIter = std::option::IntoIter<T>;
+    type IntoIter = Item<T>;
 
     /// Creates an iterator that yields the contained value if present
     /// # Examples
     ///
-    fn into_iter(self) -> IntoIter<T> {
-        IntoIter {
-            inner: Item { presence: self },
-        }
+    fn into_iter(self) -> Self::IntoIter {
+        Item { presence: self }
     }
 }
 
@@ -142,20 +190,26 @@ impl<A> Iterator for Item<A> {
 
     #[inline]
     fn next(&mut self) -> Option<Self::Item> {
-        self.presence.take()
+        match self.presence.take() {
+            Presence::Some(val) => Some(val),
+            Presence::Null | Presence::Absent => None,
+        }
     }
 
     #[inline]
-    fn size_hint(&self) -> (usize, Presence<usize>) {
+    fn size_hint(&self) -> (usize, Option<usize>) {
         let len = self.len();
-        (len, Presence::Some(len))
+        (len, Some(len))
     }
 }
 
 impl<A> DoubleEndedIterator for Item<A> {
     #[inline]
     fn next_back(&mut self) -> Option<Self::Item> {
-        self.presence.take()
+        match self.presence.take() {
+            Presence::Some(val) => Some(val),
+            Presence::Null | Presence::Absent => None,
+        }
     }
 }
 
@@ -167,46 +221,73 @@ impl<A> ExactSizeIterator for Item<A> {
 }
 
 impl<A> FusedIterator for Item<A> {}
-unsafe impl<A> TrustedLen for Item<A> {}
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Iter<'a, A> {
-    inner: Iter<&'a A>,
+    inner: Item<&'a A>,
 }
 
 impl<'a, A> Iterator for Iter<'a, A> {
     type Item = &'a A;
 
     #[inline]
-    fn next(&mut self) -> Presence<Self::Item> {
+    fn next(&mut self) -> Option<Self::Item> {
         self.inner.next()
     }
 
     #[inline]
-    fn size_hint(&self) -> (usize, Presence<usize>) {
+    fn size_hint(&self) -> (usize, Option<usize>) {
         self.inner.size_hint()
     }
 }
 
 impl<'a, A> DoubleEndedIterator for Iter<'a, A> {
     #[inline]
-    fn next_back(&mut self) -> Presence<Self::Item> {
+    fn next_back(&mut self) -> Option<Self::Item> {
         self.inner.next_back()
     }
 }
-impl<'a, A> ExactSizeIterator for Iter<'_, A> {}
+
+impl<'a, A> ExactSizeIterator for Iter<'a, A> {
+    #[inline]
+    fn len(&self) -> usize {
+        self.inner.len()
+    }
+}
 
 impl<A> FusedIterator for Iter<'_, A> {}
 
-unsafe impl<A> TrustedLen for Iter<'_, A> {}
+#[derive(Debug)]
+pub struct IterMut<'a, A> {
+    inner: Item<&'a mut A>,
+}
 
-impl<A> Clone for Iter<'_, A>
-where
-    A: Clone,
-{
-    fn clone(&self) -> Self {
-        Iter {
-            inner: self.inner.clone(),
-        }
+impl<'a, A> Iterator for IterMut<'a, A> {
+    type Item = &'a mut A;
+
+    #[inline]
+    fn next(&mut self) -> Option<Self::Item> {
+        self.inner.next()
+    }
+
+    #[inline]
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        self.inner.size_hint()
     }
 }
+
+impl<'a, A> DoubleEndedIterator for IterMut<'a, A> {
+    #[inline]
+    fn next_back(&mut self) -> Option<Self::Item> {
+        self.inner.next_back()
+    }
+}
+
+impl<'a, A> ExactSizeIterator for IterMut<'a, A> {
+    #[inline]
+    fn len(&self) -> usize {
+        self.inner.len()
+    }
+}
+
+impl<A> FusedIterator for IterMut<'_, A> {}
